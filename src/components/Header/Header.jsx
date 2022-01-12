ddd6939185f01f7aafd8ab372bbe0f9ea48ae257
nick selivanov <nick.selivanov@gmail.com>
2022-01-12 10:16:05 +0300

    add button and avatar header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
 
-import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import { AppBar, Avatar, Button, Toolbar, Typography } from '@mui/material';
 // import Box from '@mui/material/Box';
 // import Toolbar from '@mui/material/Toolbar';
 // import Typography from '@mui/material/Typography';
 // import Button from '@mui/material/Button';
 import './Header.scss';
+import avatarPicture from '../../assets/img/Avatar.png';
 
 const Header = () => {
   console.log('header');
@@ -16,11 +17,19 @@ const Header = () => {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Realworld Blog
         </Typography>
+        <Button color="success" variant="outlined" sx={{ textTransform: 'none' }}>
+          Create article
+        </Button>
+        <Typography variant="h6">John Doe</Typography>
+        <Avatar alt="Avatar" src={avatarPicture} sx={{ width: 46, height: 46 }} />
         <Button color="inherit" sx={{ textTransform: 'none', mr: 3 }}>
           Sign In
         </Button>
         <Button color="success" variant="outlined" sx={{ textTransform: 'none' }}>
           Sign Up
+        </Button>
+        <Button color="inherit" variant="outlined" sx={{ textTransform: 'none' }}>
+          Log Out
         </Button>
       </Toolbar>
     </AppBar>
