50d77a8f6c75b6b38c14134a865c9fa4f95f58f1
nick selivanov <nick.selivanov@gmail.com>
2022-01-17 17:20:33 +0300

    add go back button at SingleArticle

--- src/pages/SingleArticle.jsx
+++ src/pages/SingleArticle.jsx
@@ -1,19 +1,29 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Chip } from '@mui/material';
+import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
 import { fetchSingleArticle } from '../store/articleSlice';
 import Article from '../components/Article';
 
 const SingleArticle = () => {
   const dispatch = useDispatch();
   const { slug } = useParams();
+  const navigate = useNavigate();
+
+  const goBack = () => navigate(-1);
 
   useEffect(() => {
     dispatch(fetchSingleArticle(slug));
   }, [dispatch, slug]);
 
   const article = useSelector((state) => state.articles.singleArticle);
-  return article && <Article article={article} />;
+  return (
+    <>
+      {article && <Article article={article} />}
+      <Chip icon={<ArrowCircleLeftOutlinedIcon />} label="Go back" variant="outlined" onClick={goBack} />
+    </>
+  );
 };
 
 export default SingleArticle;
