d3933da0c5135caf68625cd72e91fbab13808687
nick selivanov <nick.selivanov@gmail.com>
2022-01-22 09:27:31 +0300

    add react-markdown

--- src/pages/CreateArticle.jsx
+++ src/pages/CreateArticle.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
@@ -6,7 +5,6 @@ import { fetchCreateArticle } from '../store/articleSlice';
 import ArticleForm from '../components/ArticleForm';
 
 const CreateArticle = () => {
-  console.log('CreateArticle');
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
